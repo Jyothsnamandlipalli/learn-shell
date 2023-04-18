256846889137bf50942c8786be4fe41fcb926336
Jyothsna <jyothsnamandlipalli@gmail.com>
2023-04-18 22:54:18 +0300

    roboshop.conf files

---
+++ variables.sh
@@ -0,0 +1,21 @@
+a=10
+name=DevOps
+
+# Print Variable
+echo a = $a
+echo name = ${name}
+
+#
+#DATE=2023-04-18
+DATE=$(date +%F)
+echo Today date is ${DATE}
+
+ARTH=$((2-3*4/2))
+
+echo ARTH = ${ARTH}
+
+echo Script Name - $0
+
+
+script_path=$(dirname $0) # directory path
+source ${script_path}/common.sh
